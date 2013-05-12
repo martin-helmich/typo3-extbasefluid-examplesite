549803df0d92607d6ceb01e2545d21a6de6045bb
Martin Helmich <m.helmich@mittwald.de>
2013-05-12 18:58:13 +0200

    Installed "helmbert_bootstrapsite" extension.

--- html/typo3conf/LocalConfiguration.php
+++ html/typo3conf/LocalConfiguration.php
@@ -14,6 +14,7 @@ return array(
 	'EXT' => array(
 		'extConf' => array(
 			'coreapi' => 'a:0:{}',
+			'helmbert_bootstrapsite' => 'a:0:{}',
 			'phpunit' => 'a:6:{s:17:"excludeextensions";s:8:"lib, div";s:10:"phpunitlib";s:0:"";s:13:"selenium_host";s:9:"localhost";s:13:"selenium_port";s:4:"4444";s:16:"selenium_browser";s:8:"*firefox";s:19:"selenium_browserurl";s:0:"";}',
 			'saltedpasswords' => 'a:2:{s:3:"FE.";a:2:{s:7:"enabled";s:1:"1";s:21:"saltedPWHashingMethod";s:28:"tx_saltedpasswords_salts_md5";}s:3:"BE.";a:2:{s:7:"enabled";s:1:"1";s:21:"saltedPWHashingMethod";s:28:"tx_saltedpasswords_salts_md5";}}',
 		),
@@ -57,6 +58,7 @@ return array(
 			'saltedpasswords',
 			'phpunit',
 			'coreapi',
+			'helmbert_bootstrapsite',
 		),
 	),
 	'FE' => array(
