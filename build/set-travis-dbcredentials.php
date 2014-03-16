4b10f41a998667051452c3c44410f7d38db2637d
Martin Helmich <kontakt@martin-helmich.de>
2014-03-16 18:04:33 +0100

    Add travis build configuration.

---
+++ build/set-travis-dbcredentials.php
@@ -0,0 +1,11 @@
+<?php
+
+$configuration = require_once __DIR__ . '/../html/typo3conf/LocalConfiguration.php';
+
+$configuration['DB']['username'] = 'travis';
+$configuration['DB']['password'] = '';
+
+file_put_contents(
+	__DIR__ . '/../html/typo3conf/LocalConfiguration.php',
+	'<?php return ' . var_export($configuration, TRUE)
+);
